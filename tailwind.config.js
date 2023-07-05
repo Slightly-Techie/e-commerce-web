4318ef3f40a006d4562286595817f131de1ba696
cedricahenkorah <cedricahenkorah@gmail.com>
2023-07-05 10:57:34 +0000

    ARIES/CA/User Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px", // Small screens and above (640px)
+      md: "768px", // Medium screens and above (768px)
+      lg: "1024px", // Large screens and above (1024px)
+      xl: "1400px", // Extra-large screens and above (1280px)
+    },
   },
   plugins: [],
-}
-
+};
