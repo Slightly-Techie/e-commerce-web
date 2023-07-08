9b58dd7e8d1ea520e64c902f4892ce9a002d5536
William M. Tsikata <tsikatawill@gmail.com>
2023-07-08 05:08:14 +0000

    add route for forgot-password

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Signup from './pages/Signup';
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
 import { Login } from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/'element = {<Signup/>} />
+        <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
