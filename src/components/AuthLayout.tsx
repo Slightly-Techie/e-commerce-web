053133943473f9f1e0ef0dd7d93a2775a009b6a4
William M. Tsikata <tsikatawill@gmail.com>
2023-07-08 05:22:11 +0000

    modify responsiveness
    hide welcome screen on devices less than 768px in width

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -2,10 +2,10 @@ import Welcome from "./Welcome";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 min-h-screen">
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
       {children}
 
-      <Welcome className="hidden sm:flex" />
+      <Welcome className="hidden md:flex" />
     </div>
   );
 };
