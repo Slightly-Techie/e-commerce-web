e270d8b72ea69d2710c047b1edb6a23956c23777
William M. Tsikata <tsikatawill@gmail.com>
2023-07-31 11:02:06 +0000

    add types file

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+import { UseFormRegister } from "./../node_modules/react-hook-form/dist/types/form.d";
+
+export enum TextSize {
+  small = "small",
+  verySmall = "verySmall",
+  body = "body",
+  large = "large",
+  heading4 = "heading4",
+  heading5 = "heading5",
+}
+
+export enum AlertType {
+  info = "info",
+  error = "error",
+  warning = "warning",
+}
+
+export enum ButtonType {
+  primary = "primary",
+  secondary = "secondary",
+  disabled = "disabled",
+}
+
+export enum ButtonSize {
+  small = "small",
+  full = "full",
+}
+
+export enum FormHelperType {
+  error = "error",
+  info = "info",
+}
+
+export type SignupStage =
+  | "enter details"
+  | "verify code"
+  | "setup account"
+  | "setup complete";
+
+type FormValues = {
+  [key: string]: unknown;
+};
+
+type RegisterFormValues = UseFormRegister<FormValues>;
+
+export type RHFInputExtension = {
+  register: RegisterFormValues;
+  required: boolean;
+};
+
+export type SignupFormFields = {
+  username: string;
+  email: string;
+  password: string;
+  agreeTerms: boolean;
+};
