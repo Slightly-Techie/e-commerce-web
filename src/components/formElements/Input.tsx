8638e96bbd8285b1309395a5baa7041d75969ed2
William M. Tsikata <tsikatawill@gmail.com>
2023-07-31 11:55:28 +0000

    make icon prop optional

--- src/components/formElements/Input.tsx
+++ src/components/formElements/Input.tsx
@@ -1,10 +1,9 @@
 import { useState, forwardRef } from "react";
 import { cn } from "../../lib/utils";
 import { HiOutlineEyeOff, HiOutlineEye } from "react-icons/hi";
-import { RHFInputExtension } from "../../types";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
 };
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -19,15 +18,18 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           isFocused ? "border-gray-500" : "border-gray300"
         )}
       >
-        <label
-          htmlFor={props.id}
-          className={cn(
-            "self-stretch grid place-content-center cursor-pointer w-8 flex-shrink-0",
-            isFocused ? "text-gray-500" : "text-gray300"
-          )}
-        >
-          {icon}
-        </label>
+        {icon && (
+          <label
+            htmlFor={props.id}
+            className={cn(
+              "self-stretch grid place-content-center cursor-pointer w-8 flex-shrink-0",
+              isFocused ? "text-gray-500" : "text-gray300"
+            )}
+          >
+            {icon}
+          </label>
+        )}
+
         <input
           {...props}
           ref={ref}
