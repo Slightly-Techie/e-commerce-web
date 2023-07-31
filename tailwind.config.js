7edde3471855e7249b19934aedd63a91e4851a06
William M. Tsikata <tsikatawill@gmail.com>
2023-07-31 10:56:53 +0000

    add slow spin animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
       fontFamily: {
         "product-sans": "Product Sans",
         anton: "Anton",
