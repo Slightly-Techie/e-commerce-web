cbdc3023d94f2a9b50cef554544845ad922779d6
William M. Tsikata <tsikatawill@gmail.com>
2023-07-31 11:04:23 +0000

    create AccountSetupComplete cmp
    This component is displayed on successful account setup
    
    **Todo**: Add confetti pop animation

---
+++ src/components/AccountSetupComplete.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import SetupAccountLayout from "./SetupAccountLayout";
+import { ButtonType } from "../types";
+
+const AccountSetupComplete = () => {
+  const navigate = useNavigate();
+
+  return (
+    <SetupAccountLayout
+      title="Setup Complete"
+      description="Welcome to Slightly Techie E-commerce space"
+      icon={
+        <img
+          className="w-[107px] mx-auto"
+          src="/public/assets/icons/done.svg"
+          alt="..."
+        />
+      }
+    >
+      <Button btnType={ButtonType.primary} onClick={() => navigate("/")}>
+        Start shopping now
+      </Button>
+    </SetupAccountLayout>
+  );
+};
+
+export default AccountSetupComplete;
