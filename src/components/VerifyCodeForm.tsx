6e0332d25000b3b426ec4bda10750cc8b05182d3
William M. Tsikata <tsikatawill@gmail.com>
2023-09-21 13:40:57 +0000

    add resend verfication code feature

--- src/components/VerifyCodeForm.tsx
+++ src/components/VerifyCodeForm.tsx
@@ -11,8 +11,9 @@ import { hideEmail } from "../lib/utils";
 import { useSignupStageStore } from "../store/signupStageStore";
 import { useUserStore } from "../store/userStore";
 import { useMutation } from "@apollo/client";
-import { VERIFY_CODE } from "../lib/queries";
+import { RESEND_VERFICATION, VERIFY_CODE } from "../lib/queries";
 import { useAlertStore } from "../store/alertStore";
+import { useEffect, useState } from "react";
 
 type FormValues = {
   code: string;
@@ -26,16 +27,32 @@ const VerifyCodeForm = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
+  const [resendTime, setResendTime] = useState(0);
+
   const { changeStage } = useSignupStageStore();
   const { user, updateToken } = useUserStore();
   const { showAlert } = useAlertStore();
 
   const [verifyCode, { loading }] = useMutation(VERIFY_CODE);
+  const [resendVCode, { loading: sendingVCode }] =
+    useMutation(RESEND_VERFICATION);
+
+  const convertTime = (time: number): { mins: string; secs: string } => {
+    const calcMins = Math.floor(time / 60);
+    const calcSecs = time % 60;
+
+    const padd = (_: number): string => {
+      return String(_).padStart(2, "0");
+    };
+
+    return { mins: padd(calcMins), secs: padd(calcSecs) };
+  };
 
   const onSubmit: SubmitHandler<FormValues> = (formData) => {
     verifyCode({
       variables: { input: { token: formData.code } },
     }).then(({ data }) => {
+      setResendTime(30);
       if (data.confirmEmail.success) {
         showAlert({
           alertType: AlertType.info,
@@ -56,6 +73,47 @@ const VerifyCodeForm = () => {
       }
     });
   };
+
+  const handleResendVerification = () => {
+    setResendTime(30);
+    resendVCode({ variables: { input: { email: user?.email } } })
+      .then(({ data: { resendEmailOtp } }) => {
+        if (resendEmailOtp.success) {
+          showAlert({
+            alertType: AlertType.success,
+            alertText: "New token has been sent to your email",
+          });
+        }
+        if (resendEmailOtp.errors) {
+          resendEmailOtp.errors.forEach(({ message }: { message: string }) => {
+            showAlert({
+              alertType: AlertType.error,
+              alertText: message,
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        console.log({ err });
+        showAlert({
+          alertType: AlertType.error,
+          alertText: "Request failed",
+        });
+      });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (resendTime > 0) {
+        setResendTime((prev) => {
+          if (prev > 0) return prev - 1;
+          return 0;
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [resendTime]);
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)} title="Get code from email">
@@ -87,15 +145,34 @@ const VerifyCodeForm = () => {
         )}
       </InputGroup>
 
-      <Button
-        disabled={loading}
-        btnType={
-          errors.code || loading ? ButtonType.disabled : ButtonType.primary
-        }
-        className="w-full"
-      >
-        Verify
-      </Button>
+      {resendTime > 0 && (
+        <p>Resend code after {convertTime(resendTime).secs} seconds</p>
+      )}
+
+      <div className="flex gap-3">
+        <Button
+          type="button"
+          btnType={
+            sendingVCode || resendTime > 0
+              ? ButtonType.disabled
+              : ButtonType.secondary
+          }
+          className="w-full"
+          onClick={handleResendVerification}
+        >
+          Resend code
+        </Button>
+
+        <Button
+          type="submit"
+          btnType={
+            errors.code || loading ? ButtonType.disabled : ButtonType.primary
+          }
+          className="w-full"
+        >
+          Verify
+        </Button>
+      </div>
     </Form>
   );
 };
